8aef1ad3fcf88feba09c041ea7ca82e374bf4d40
mustafacoskunn <l1621012067@stud.sdu.edu.tr>
2019-08-09 00:41:39 +0300

    mongodb baglantısı
    mlab ile baglantı

---
+++ helper/db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+module.exports=()=>{
+   mongoose.connect('mongodb://movie_user:123456a@ds213615.mlab.com:13615/movie-api',{ useNewUrlParser: true });
+   mongoose.connection.on('open',()=>[
+       console.log('MongoDB Baglantısı Başarılı.')
+   ]);
+    mongoose.connection.on('error',(err)=>[
+        console.log('MongoDB Baglantısı Başarısız.',err)
+    ]);
+
+};
