092ed23c3f2dcfcdbbb1e7eb38817e9aff077183
mustafacoskunn <l1621012067@stud.sdu.edu.tr>
2019-08-09 00:52:54 +0300

    Movie Şeması oluşturuldu

---
+++ Models/Movie.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+
+const MovieSchema=new Schema({ // movie şemamızı oluşturuyoruz
+
+    title:{
+        type : String,
+        required : true
+    },
+    category : String,
+    country : String,
+    year : Number,
+    imbd_score : Number,
+    director_id : Schema.Types.ObjectId
+
+});
+
+mongoose.model('moview',MovieSchema); // şemayı oluşturduk ve dışarı aktardık.
