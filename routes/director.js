44896953a36c0340fd0e96fe9dc0a58ed6118638
mustafacoskunn <l1621012067@stud.sdu.edu.tr>
2019-08-10 00:33:17 +0300

    /api/directory/directory_id endpoint

--- routes/director.js
+++ routes/director.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose=require('mongoose');
 const router = express.Router();
 
 
@@ -72,7 +73,66 @@ router.get('/', (req, res, next)=> {
   });
 
 });
+//tek bir yonetmeni filmleri ile beraber göstermek için
+router.get('/:directory_id', (req, res, next)=> {
 
+  const promise=Director.aggregate([ //join yapcagımız için aggregate kullanıyoruz
+    {
+      $match:{ // ide göre listele bir nevi sorgu
+        '_id': mongoose.Types.ObjectId(req.params.directory_id) //'_id' fieldındaki ve girilen id eşit olanı getir
+      },
+    },
+    {
+
+      $lookup:{
+        from: 'movies', //nereis ile join olucak
+        localField: '_id', //director tablosundaki ne ile eşleşicek
+        foreignField: 'director_id',//movie tablosundaki ne ile birleşcek
+        as: 'movies' //hangi degişkene atanacak unwind kısmında bu değişkeni alıcagız
+
+      }
+    },
+    {
+      $unwind:{
+        path: '$movies', //as: deki moviesi alıyoruz
+        preserveNullAndEmptyArrays: true //bunu true yapmassak sadece filmi olan yönetmenler gelir o yüzden true yapıyoruz
+
+      }
+    },
+    {
+      $group:{ //şimdi yönetmenin 2 3 tane filmi olsa bile ayrı ayrı gösteriyor onları gruplamak için
+        _id: {
+          _id:'$id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies:{
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      $project:{ //_id şeklinde gösteriyordu yönetmenleri onu düzenledik
+        _id: '$_id._id',
+        name : '$_id.name',
+        surname : '$_id.surname',
+        bio : '$_id.bio',
+        movies: '$movies'
+      }
+    }
+
+
+  ]);
+
+
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+
+});
 
 
 
